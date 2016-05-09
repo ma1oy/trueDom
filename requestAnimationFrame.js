356e36f202acd352e46d5503cb17308ad5f184f0
roman <sheva2012h@gmail.com>
2016-05-10 00:41:00 +0300

    Add animation frame polyfill

---
+++ requestAnimationFrame.js
@@ -0,0 +1,24 @@
+;+function($) {
+    var performance = performance || { now: function() { return new Date().getTime(); } };
+
+    let lastTime = 0, vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !$.requestAnimationFrame; ++x) {
+        $.requestAnimationFrame = $[vendors[x]+'RequestAnimationFrame'];
+        $.cancelAnimationFrame = $[vendors[x]+'CancelAnimationFrame']
+            || $[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!$.requestAnimationFrame)
+        $.requestAnimationFrame = function(callback/*, element*/) {
+            let currTime = performance.now(),
+                timeToCall = lastTime - currTime + 16 > 0 || 0;
+            var id = $.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!$.cancelAnimationFrame)
+        $.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}(window);
